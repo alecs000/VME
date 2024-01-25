1e404da773f9241a43b80de3122cbf3067df5031
alecs000 <alecs.2004fed@gmail.com>
2024-01-26 01:29:35 +0300

    Particle Appear

--- Assets/Game/Scripts/Minigames/PersonnelLogic/JobsView.cs
+++ Assets/Game/Scripts/Minigames/PersonnelLogic/JobsView.cs
@@ -6,12 +6,19 @@ using UnityEngine.UI;
 public class JobsView : MonoBehaviour
 {
     [SerializeField] private FadeAnimatedPanel view;
+    [SerializeField] private ParticleSystem particleSystemLogo;
+    [SerializeField] private Button close;
     private void Start()
     {
-        
+        close.onClick.AddListener(Deactivate);
     } 
     public void Activate()
     {
         view.Appear();
+        particleSystemLogo.Play();
+    }
+    private void Deactivate()
+    {
+        view.Disappear();
     }
 }
