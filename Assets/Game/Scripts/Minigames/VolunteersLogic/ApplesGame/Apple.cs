3a88e7d33295cf91c5249914af8d5d4d4f79d77b
alecs000 <alecs.2004fed@gmail.com>
2024-02-29 16:20:19 +0300

    Apple

--- Assets/Game/Scripts/Minigames/VolunteersLogic/ApplesGame/Apple.cs
+++ Assets/Game/Scripts/Minigames/VolunteersLogic/ApplesGame/Apple.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class Apple : MonoBehaviour
 {
     [SerializeField] private int amount;
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody2D apple;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out PlayerAppleGame player))
         {
             player.AddApple(amount);
         }
+    }
+    private void FixedUpdate()
+    {
+        apple.MovePosition(new Vector2(transform.position.x, transform.position.y) + Vector2.down * Time.fixedDeltaTime * speed);
     }
 }
--- Assets/Game/Scripts/Minigames/VolunteersLogic/ApplesGame/AppleGame.cs
+++ Assets/Game/Scripts/Minigames/VolunteersLogic/ApplesGame/AppleGame.cs
@@ -8,17 +8,29 @@ public class AppleGame : MonoBehaviour
     [SerializeField] private VisualNovel visualNovel;
     [SerializeField] private DialogSO startDialog;
     [SerializeField] private DialogSO dialogComplete;
+    [SerializeField] private Apple prefab;
+    [SerializeField] private int count;
+    [SerializeField] private Vector2 appleDelay;
+    [SerializeField] private Vector2 xRange;
+    [SerializeField] private float yPosition;
 
     private PoolMono<Apple> apples;
     private int _applesCollected;
 
     private void Start()
     {
+        apples = new PoolMono<Apple> (prefab, count);
         visualNovel.StartNovel(startDialog, StartGame, true);
     }
     private void StartGame()
     {
-
+        StartCoroutine(Spawn());
+    }
+    private IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(Random.Range(appleDelay.x, appleDelay.y));
+        Apple apple= apples.GetFreeElement();
+        apple.transform.position = new Vector2(Random.Range(xRange.x, xRange.y), yPosition);
     }
     public void AddApple(int amount)
     {
